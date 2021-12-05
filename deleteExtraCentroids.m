05ef1e7229e4181455595c4ce3099d1516a4f970
orkunvural <vuralorkun@gmail.com>
2021-12-06 01:22:05 +0300

    new files

---
+++ deleteExtraCentroids.m
@@ -0,0 +1,16 @@
+function [newCentroids] = deleteExtraCentroids(preCentroids,postCentroids)
+%DELETEEXTRACENTROIDS Summary of this function goes here
+%   Detailed explanation goes here
+    if isempty(preCentroids)
+        newCentroids=preCentroids;
+    else if(length(preCentroids(:,1))-length(postCentroids(:,1))>0)  
+        newCentroids = zeros(length(postCentroids(:,1)),2);
+        for i = 1:length(postCentroids(:,1))
+            [~,index] = min(abs(preCentroids - postCentroids(i)));
+            newCentroids(i,:) = preCentroids(index(1),index(2));
+        end
+    else  
+        newCentroids=preCentroids;
+    end
+end
+
